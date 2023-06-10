187ce25b6bc0d84eff22588eb227412c2384f5fb
damiru0987 <136152950+damiru0987@users.noreply.github.com>
2023-06-10 13:15:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,16 +23,16 @@ global.APIKeys = {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 global.session = process.env.SESSION_ID || 'PUT HERE',
 global.ownername = process.env.OWNER_NAME === undefined ? "DarkWinzo" : process.env.OWNER_NAME
-let fake = process.env.OWNER_NUMBER === undefined ? '94775200935' : process.env.OWNER_NUMBER
+let fake = process.env.OWNER_NUMBER === undefined ? '947704752135' : process.env.OWNER_NUMBER
 global.owner = [`${fake}`]
 global.ownernumber = [`${fake}`]
 global.premium = [`${fake}`]
 global.anticall = process.env.ANTICALL === undefined ? 'false' : process.env.ANTICALL
 global.packname = process.env.PACK_NAME === undefined ? 'Queen-MD' : process.env.PACK_NAME
 global.author = process.env.STICKER_AUTHOR_NAME === undefined ? 'DarkWinzo' : process.env.STICKER_AUTHOR_NAME
-global.pmblock = process.env.PM_BLOCK === undefined ? 'false' : process.env.PM_BLOCK
+global.pmblock = process.env.PM_BLOCK === undefined ? 'true' : process.env.PM_BLOCK
 global.chatbot_pm = process.env.PM_CHATBOT === undefined ? 'false' : process.env.PM_CHATBOT
-global.chatbot_group = process.env.GROUP_CHATBOT === undefined ? 'false' : process.env.GROUP_CHATBOT
+global.chatbot_group = process.env.GROUP_CHATBOT === undefined ? 'true' : process.env.GROUP_CHATBOT
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 global.Botname = 'Q'+'ᴜ'+'ᴇ'+'ᴇ'+'ɴ'+'-'+'ᴍ'+'ᴅ'
